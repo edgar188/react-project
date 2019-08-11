d12f3b1d2bd99f9a171b0528f37c5f562f183248
Edgar Hovhannisyan <edgar.hovhannisyan08@gmail.com>
2019-08-11 13:41:17 +0400

    Updates

---
+++ precache-manifest.256a0a394b7b91212e79ec39f45f0fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e202670a2bde1d9b2639be32207686",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "d7a0666ead3f31834657",
+    "url": "/react-project/static/css/main.89a2ba17.chunk.css"
+  },
+  {
+    "revision": "0e851f038b4ec7daa696",
+    "url": "/react-project/static/js/2.81a2551c.chunk.js"
+  },
+  {
+    "revision": "d7a0666ead3f31834657",
+    "url": "/react-project/static/js/main.2fbe8630.chunk.js"
+  },
+  {
+    "revision": "c38e84bee75be2db3df6",
+    "url": "/react-project/static/js/runtime~main.270f79fb.js"
+  },
+  {
+    "revision": "b11c7feb6108dadee0724938d09e74a8",
+    "url": "/react-project/static/media/bg.b11c7feb.jpg"
+  },
+  {
+    "revision": "8394ab2b9d3474b98b9c8d32801c5563",
+    "url": "/react-project/static/media/logo.8394ab2b.png"
+  }
+]);
